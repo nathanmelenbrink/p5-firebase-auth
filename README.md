a3623f70ab7ba9a2d204de65a903ac6eb4922ce7
Nathan Melenbrink <nathanmelenbrink@users.noreply.github.com>
2018-03-30 23:59:32 -0400

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # p5-firebase-auth
 Basic functionality for creating a Firebase app with authentication and a p5 front-end 
+
+Bits and pieces taken from Google demo code and Daniel Shiffman's Firebase tutorial
+
+In order to run the demo: 
+
+1. Replace API key and database URL
---
+++ sketch.js
@@ -0,0 +1,60 @@
+let container, canv, input, dateInput, button; 
+let loggedInUser;
+
+function setup(){
+	container = createElement('div').id('container');
+	canv = createCanvas(600, 400).parent(container);
+	input = createInput('beverages').parent(container);
+
+	dateInput = document.createElement("INPUT");
+    dateInput.setAttribute("type", "date");
+    dateInput.setAttribute("value", new Date());
+    dateInput.setAttribute("id", 'dateInput');
+    document.getElementById('container').appendChild(dateInput);
+
+	button = createButton('submit').parent(container);
+	button.mousePressed(submitToFirebase);
+	container.hide(); // don't show by default, wait until user signs in 
+}
+
+function plotChart(){
+	background(200);
+	let entries;
+	return firebase.database().ref('/users/' + loggedInUser.uid).once('value').then(function(snapshot) {
+		entries = snapshot.val().entries;
+		let keys = Object.keys(entries);
+		for (let i = 0; i < keys.length; i++) {
+			let key = keys[i];
+			let entry = entries[key];
+		    console.log(entry);
+		    console.log(key);
+		    let x = (width / keys.length) * i; 
+		    let y = (height/5) * entry.bevs;
+		    ellipse(x, y, 20, 20);
+		}
+	});
+	console.log(entries);
+
+}
+
+function loginUser(){
+	loggedInUser = firebase.auth().currentUser;
+	container.show();
+	
+	plotChart();
+}
+
+function logoutUser(){
+	container.hide();
+}
+
+function submitToFirebase(){
+	let day = document.getElementById('dateInput').value;
+	let val = parseInt(input.value());
+	input.value('');
+
+  	firebase.database().ref('users/' + loggedInUser.uid + '/entries/' + day).set({
+		bevs: val
+        //some more entry data
+    });
+}
